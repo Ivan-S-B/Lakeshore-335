d193924fcba045f2307b9fd1084ce0760056c488
Ivan S. Bespalov <64466407+Ivan-S-B@users.noreply.github.com>
2025-05-26 11:59:46 -0700

    Update Run_All_Scripts.py

--- Run_All_Scripts.py
+++ Run_All_Scripts.py
@@ -2,7 +2,7 @@ import subprocess
 
 # Start both scripts simultaneously
 process1 = subprocess.Popen(["python", "Read_Temperature_from_Lake_Shore_335_with_GUI_with_csv_V28.py"])
-process2 = subprocess.Popen(["python", "Lake_Shore_Model_335_Heater_with_GUI_Two_Heaters_Option_PID_V8.py"])
+process2 = subprocess.Popen(["python", "Lake_Shore_335_Heater_Control.py"])
 process3 = subprocess.Popen(["python","Check_GRIB_Hardware_with_GUI_V3.py"])
 # Wait for both scripts to complete
 process1.wait()
